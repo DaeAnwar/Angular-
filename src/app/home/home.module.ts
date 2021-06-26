74043b7f42465105e4ad392c6bb194a86cad1664
today is a good day to die <hamzajazyri@gmail.com>
2021-06-26 21:30:25 +0100

    ++

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -9,6 +9,7 @@ import { MenuModule } from '../shared/menu/menu.module';
 import { EventsBlockComponent } from './src/components/events-block/events-block.component';
 import { BookTableModalComponent } from './src/components/book-table-modal/book-table-modal.component';
 import { ContactUsComponent } from './src/components/contact-us/contact-us.component';
+import { GalleryComponent } from './src/components/gallery/gallery.component';
 
 const routes : Routes = [
   {
@@ -23,7 +24,8 @@ const routes : Routes = [
     CarrasolComponent,
     EventsBlockComponent,
     BookTableModalComponent,
-    ContactUsComponent
+    ContactUsComponent,
+    GalleryComponent
   ],
   imports: [
     CommonModule,
